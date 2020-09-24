272d9a65efa1d4f6c4a6fe01d2ea39affad5c42f
Alexander Bowers <7319abowers@gmail.com>
2020-09-24 23:21:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    puts "Fizzbuz"
+  elsif  int % 3 == 0  && int % 5 != 0
+    puts "Fizz"
+  elsif int % 3 != 0 && int % 5 == 0
+    puts "Buzz"
+  else
+    return nil
+  end
 
 end
