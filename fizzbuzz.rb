81070f217194b8161a34f4f4ababb80873f4dad3
Alexander Bowers <7319abowers@gmail.com>
2020-09-24 23:26:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,13 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     puts "Fizzbuzz"
+    return "Fizzbuzz"
   elsif  int % 3 == 0  && int % 5 != 0
     puts "Fizz"
+    return "Fizz"
   elsif int % 3 != 0 && int % 5 == 0
     puts "Buzz"
+    return "Buzz"
   else
     return nil
   end
